3e2da4493c031d1d81eff785cab76bf6aaeaa6a1
Swatheswari R <swatheswari2005@gmail.com>
2025-04-22 15:05:17 +0000

    feat:addes controller logic

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express =require("express")
 const cors =require ("cors");
 const dotenv=require("dotenv");
 const connectDB= require("./config/db")
+const toadoRoutes=require("./views/todoRoutes")
 
 dotenv.config();
 connectDB();
@@ -12,6 +13,8 @@ app.get("/",(req,res)=>{
     res.json("Swathu");
 
 })
+
+app.use("/api/todos",toadoRoutes)
 const PORT= process.env.PORT || 5000;
 app.listen(PORT,()=>{
     console.log(`Server running at port ${PORT}`);
