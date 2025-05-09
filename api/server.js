19fe3949e9247c5b7016a86bfa724858f1d5ee94
Swatheswari R <swatheswari2005@gmail.com>
2025-05-09 13:25:58 +0000

    backend started

--- api/server.js
+++ api/server.js
@@ -1,34 +1,16 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const connectDB = require("./config/db");
-const todoRoutes = require("./views/todoRoutes");
-const todoController=require("./controllers/todoController");
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
 
 dotenv.config();
-connectDB();
 
 const app = express();
 
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.json("Swathu");
+app.get('/', (req, res) => {
+  res.json('hello');
 });
 
-// Use todoRoutes for API endpoints
-app.use("/api/todos", todoRoutes);
-
-// Error handling middleware
-app.use((err, req, res, next) => { 
-    console.error(err.stack); // log the actual error object
-    res.status(500).json({ message: "Something went wrong" });
-});
-
-// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running at port ${PORT}`);
+  console.log(`Server running at PORT ${PORT}`);
 });
